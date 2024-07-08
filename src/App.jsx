3f5502aefdedf4c202cef8553316896db3bbc857
JuAhLee <124784742+JuAhLee@users.noreply.github.com>
2024-07-08 16:00:36 +0900

    PokeDex-AxiosData

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,45 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  
+  const [pokemons, setpokemons] = useState([]);
+
+  const url = "https://pokeapi.co/api/v2/pokemon/?limit=1008&offset=0";
+
+  useEffect(() => {
+    fetchPokeData();
+  }, [pokemons]);
+
+  const fetchPokeData = async () => {
+    try {
+      const response = await axios.get(url);
+      console.log(response.data.results);
+      setpokemons(response.data.results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline"> Hello</h1>
-    </div>
+    <article className="pt-6">
+      <header className="flex flex-col gap-2 w-full px-4 z-50 ">
+        Input form
+      </header>
+      <section className="pt-6 flex flex-col justify-center items-center overflow-auto z-0">
+        <div className="flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl b">
+          {pokemons.length > 0 ? 
+          (
+            pokemons.map(({ url, name }, index) => <div>{name}</div>)
+          ) : 
+          (
+            <h2 className="font-medium text-lg text-slate-900 mb-1">포켓몬이 없습니다.</h2>
+          )}
+        </div>
+      </section>
+    </article>
   );
 }
 
