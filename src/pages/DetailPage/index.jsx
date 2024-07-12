3ec156255a833e748ee05bfddcadaf75fa230886
JuAhLee <124784742+JuAhLee@users.noreply.github.com>
2024-07-12 12:10:29 +0900

    DetailPage_Axios-get-mainurl

--- src/pages/DetailPage/index.jsx
+++ src/pages/DetailPage/index.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const DetailPage = () => {
+  const params = useParams();
+  const pokemonId = params.id;
+
+  useEffect(() => {
+    fetchPokemonData();
+  }, []);
+
+  async function fetchPokemonData() {
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
+
+    try {
+      const { data: pokemonData } = await axios.get(url);
+      console.log(pokemonData);
+    } catch (error) {
+      console.error(error);
+    }
+  }
   return <div>DetailPage</div>;
 };
 
