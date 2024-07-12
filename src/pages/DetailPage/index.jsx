4021407d57ace0c2ed5966ac18e0b9b2a5b55f00
JuAhLee <124784742+JuAhLee@users.noreply.github.com>
2024-07-12 15:20:25 +0900

    DetailPage_Fomatted Data

--- src/pages/DetailPage/index.jsx
+++ src/pages/DetailPage/index.jsx
@@ -21,12 +21,55 @@ const DetailPage = () => {
         const { name, id, types, weight, height, stats, abilities } =
           pokemonData;
         const nextAndPreviousPokemon = await getNextAndPreviousPokemon(id);
-        console.log(nextAndPreviousPokemon);
+
+        const DamageRelations = await Promise.all(
+          types.map(async (i) => {
+            const type = await axios.get(i.type.url);
+            // console.log(type);
+
+            return type.data.damage_relations;
+          })
+        );
+
+        const formattedPokemonData = {
+          id: id,
+          name: name,
+          weight: weight / 10,
+          height: height / 10,
+          previous: nextAndPreviousPokemon.previous,
+          next: nextAndPreviousPokemon.next,
+          abilities: formatPokemonAbilites(abilities),
+          stats: formatPokemonStats(stats),
+          DamageRelations: DamageRelations,
+        };
+        console.log(formattedPokemonData);
       }
     } catch (error) {
       console.error(error);
     }
   }
+
+  const formatPokemonStats = ([
+    statHP,
+    statATK,
+    statDEP,
+    statSATK,
+    statSDEP,
+    statSPD,
+  ]) => [
+    { name: "Hit Points", baseStat: statHP.base_stat },
+    { name: "Attack", baseStat: statATK.base_stat },
+    { name: "Defense", baseStat: statDEP.base_stat },
+    { name: "Special Attack", baseStat: statSATK.base_stat },
+    { name: "Special Defense", baseStat: statSDEP.base_stat },
+    { name: "Speed", baseStat: statSPD.base_stat },
+  ];
+
+  const formatPokemonAbilites = (abilities) => {
+    return abilities
+      .filter((_, index) => index <= 1)
+      .map((obj) => obj.ability.name.replaceAll(("-", " ")));
+  };
 
   async function getNextAndPreviousPokemon(id) {
     const urlPokemon = `${baseUrl}?limit=1&offset=${id - 1}`;
