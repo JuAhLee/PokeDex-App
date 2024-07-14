f6290034fda01185f1937dbd4372b275f3c16617
JuAhLee <124784742+JuAhLee@users.noreply.github.com>
2024-07-14 15:58:02 +0900

    DetailPage_DamageRelation-DamageDataFormatting(PostDamageValue)

--- src/components/DamageRelations.jsx
+++ src/components/DamageRelations.jsx
@@ -7,8 +7,39 @@ const DamageRelations = ({ damages }) => {
     const arrayDamage = damages.map((damage) =>
       separateObjectBetweenToAndFrom(damage)
     );
+
+    if (arrayDamage.length === 2) {
+      //합치는 부분
+    } else {
+      postDamageValue(arrayDamage[0].from);
+    }
   }, []);
 
+  // type이 1개인 데미지 관계
+  const postDamageValue = (props) => {
+    const result = Object.entries(props).reduce((acc, [keyName, value]) => {
+      const key = keyName;
+
+      const valuesOfKeyName = {
+        double_damage: "2X",
+        half_damage: "1/2x",
+        no_damage: "0x",
+      };
+
+      return (acc = {
+        [keyName]: value.map((i) => ({
+          // console.log(i)  속성
+          // console.log(valuesOfKeyName[key])  데미지 값 (ex. 2x)
+          damageValue: valuesOfKeyName[key],
+          ...i,
+        })),
+        ...acc,
+      });
+    }, {});
+    console.log("re", result);
+  };
+
+  //   데이터 가공1 (_from, _to 순서로 합치기)
   const separateObjectBetweenToAndFrom = (damage) => {
     const from = filterDamageRelations("_from", damage);
 
