a5986097a7f7a7be27604c02334152ba20f0ee48
JuAhLee <124784742+JuAhLee@users.noreply.github.com>
2024-07-15 20:30:10 +0900

    Detail_DamageRelation-UI-Modal

--- src/pages/DetailPage/index.jsx
+++ src/pages/DetailPage/index.jsx
@@ -10,10 +10,12 @@ import { Vector } from "../../assets/Vector";
 import Type from "../../components/Type";
 import BaseStat from "../../components/BaseStat";
 import DamageRelations from "../../components/DamageRelations";
+import DamageModal from "../../components/DamageModal";
 
 const DetailPage = () => {
   const [pokemon, setPokemon] = useState();
   const [isLoading, setIsLoading] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const params = useParams();
   const pokemonId = params.id;
@@ -173,7 +175,8 @@ const DetailPage = () => {
               height="auto"
               loading="lazy"
               alt={pokemon.name}
-              className="object-contain h-full"
+              className={"object-contain h-full"}
+              onClick={() => setIsModalOpen(true)}
             />
           </div>
         </section>
@@ -236,16 +239,23 @@ const DetailPage = () => {
           </div>
 
           {/* Modal */}
-          {pokemon.DamageRelations && (
+          {/* {pokemon.DamageRelations && (
             <div className="w-10/12">
               <h2 className={`text-base text-center font-semibold ${text}`}>
                 <DamageRelations damages={pokemon.DamageRelations} />
               </h2>
-              데미지
             </div>
-          )}
+          )} */}
         </section>
+
+        {/* DamageRelation-Modal */}
       </div>
+      {isModalOpen && (
+        <DamageModal
+          setIsModalOpen={setIsModalOpen}
+          damages={pokemon.DamageRelations}
+        />
+      )}
     </article>
   );
 };
