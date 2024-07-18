7c8753db48d921c936173632ea13d7c762545b77
JuAhLee <124784742+JuAhLee@users.noreply.github.com>
2024-07-18 19:43:40 +0900

    DetailPage_DamageRelation-useOnClickOutside Hook 생성

---
+++ src/hooks/useOnClickOutside.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+
+export default function useOnClickOutside(ref, handler) {
+  useEffect(() => {
+    const listener = (event) => {
+      // 모달 안 클릭
+      //   console.log(event.target);
+
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+
+      // 모달 밖 클릭
+      handler();
+    };
+
+    document.addEventListener("mousedown", listener);
+
+    return () => {
+      document.removeEventListener("mousedown", listener);
+    };
+  }, [ref, handler]);
+}
